91281780f7b4715290e37cfbe2f66844373cc4fc
ThatoGafane <thatojoshua5@gmail.com>
2023-02-26 14:32:44 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/Thatogafane/chatly
+
+go 1.19
+
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/ably/ably-go v1.2.11 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/ugorji/go/codec v1.2.10 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/ably/ably-go/ably"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strings"
+)
+
+//Connect to ably
+
+//Connect to a channel(chat room)
+
+//Publish a message to everyone in the channel(chat room)
+
+//Show if a user left the channel(chat room)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	username := os.Args[1]
+
+	client, err := ably.NewRealtime(
+		ably.WithKey(os.Getenv("ABLY_API_KEY")),
+		ably.WithClientID(username),
+		ably.WithEchoMessages(false),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic := client.Channels.Get("Fun Golang")
+
+	_, _ = topic.SubscribeAll(context.Background(), func(message *ably.Message) {
+		fmt.Printf("Recieved message from %v: '%v\n", message.ClientID, message.Data)
+	})
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		text, _ := reader.ReadString('\n')
+
+		text = strings.ReplaceAll(text, "\n", "")
+
+		err = topic.Publish(context.Background(), "Fun Golang", text)
+		if err != nil {
+			err = fmt.Errorf("publishing to topic: %w", err)
+			fmt.Println(err)
+		} else {
+			fmt.Printf("Published message: %v\n", text)
+		}
+	}
+}
